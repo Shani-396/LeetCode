948624a35b87b86cd834b61bb996e166e2b667f9
Shani-396 <4171396sg@gmail.com>
2026-01-20 18:28:18 +0200

    Time: 3 ms (36.82%), Space: 19.4 MB (16.58%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        openBrackets = ['(', '[', '{']
+        closedBrackets = [')', ']', '}']
+        dict = { '(': ')', '[': ']', '{': '}' }
+        stack = []
+        for char in s:
+            if char in openBrackets:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if dict[stack.pop()] != char:
+                    return False
+        return len(stack) == 0
+        
