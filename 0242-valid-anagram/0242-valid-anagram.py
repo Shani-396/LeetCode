19501d68c263e254f8d90986aa17bb0c28b6e892
Shani-396 <4171396sg@gmail.com>
2026-03-12 23:02:42 +0200

    Time: 15 ms (42.81%), Space: 19.3 MB (98.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        my_dict = {}
+        for char in s:
+            if char in my_dict:
+                my_dict[char] +=1
+            else:
+                my_dict[char] = 1
+        for char in t:
+            if char in my_dict and my_dict[char] != 0:
+                my_dict[char] -= 1
+            else:
+                return False
+        return True
